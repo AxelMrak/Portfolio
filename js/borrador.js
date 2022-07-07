b73488ffcc68846bf86c85aa67349a9e7082e411
Axel Sarmiento <96851183+AxelMrak@users.noreply.github.com>
2022-07-07 12:55:25 -0300

    Add files via upload

---
+++ js/borrador.js
@@ -0,0 +1,11 @@
+$(".it").click(() => {
+  
+    $(".about")
+      .replaceWith(`Il mio nome è Axel, vengo da Argentina, adesso vivo in Italy. Sono autodidatta. La mia formazione è l'indagine personale, corsi, e Open Bootcamp (organizzazione di educazione in Spain). Non ho esperienza in questo campo di lavoro, per ricerco una opportunità. Ho conoscenza in HTML, CSS, JavaScript, React, e libraries come Bootstrap. Sempre voglio e ricerco dare lo migliore di me in i progetti.`);
+  });
+  
+  $(".es").click(() => {
+    $(".about").replaceWith(
+      `Mi nombre es Axel, vengo de Argentina, ahora vivo en Italia. Soy autodidacta, mi formación se basa en la investigacion personal, cursos y Open Bootcamp (organización educativa en España). No tengo experiencia en este campo laboral, por lo que estoy buscando una oportunidad para comenzar. Tengo conocimientos en HTML, CSS, JavaScript, React y librerias como Bootstrap. Quiero y busco dar lo mejor de mí en los proyectos en los cuales participe.`
+    );
+  });
